c8349e14b0f7d2c1a9c8f3d08256ac9d19a61ea7
kriss <ksandoval@cerveros.cl>
2024-02-16 11:46:33 -0300

    first commit

---
+++ components/UserNav.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { signIn, signOut, useSession, getProviders  } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+export default function UserNav() {
+  const { data: session } = useSession();
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+
+    console.log("session?.user", session?.user);
+  }, []);
+  
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-10 w-10 rounded-sm">
+          <Avatar className="h-10 w-10 rounded-sm">
+            <AvatarImage src="https://nkljkwikhlggqlnjctge.supabase.co/storage/v1/object/public/user%20image/avatar.png" />
+            <AvatarFallback className="rounded-sm">Kris</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel>
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">Kris</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              ksandoval@gmail.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>Sign out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
